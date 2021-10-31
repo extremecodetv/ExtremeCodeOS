faee31415b32fe3fb45fc22b12e47b51cf2c69fb
KirillAldashkin <a89086520092k@yandex.ru>
2021-10-31 22:53:15 +0800

    Initial commit for high-speed math library

---
+++ src/Libraries/Math/CompareService.cs
@@ -0,0 +1,128 @@
+﻿namespace Math
+{
+    /// <summary>
+    /// Сервис, реализующий сравнение чисел.
+    /// </summary>
+    public class CompareService
+    {
+        /// <summary>
+        /// Константа, обозначающая, что левое число равно правому.
+        /// </summary>
+        public const int EQUALS = 0xFACE;
+        /// <summary>
+        /// Константа, обозначающая, что левое число больше правого.
+        /// </summary>
+        public const int BIGGER = 0xDEAD;
+        /// <summary>
+        /// Константа, обозначающая, что левое число меньше правого.
+        /// </summary>
+        public const int SMALLER = 0xBEEF;
+        /// <summary>
+        /// Сравнивает два числа
+        /// </summary>
+        /// <param name="left">Левое число</param>
+        /// <param name="right">Правое число</param>
+        /// <returns>Одна из констант <see cref="SMALLER"/>, <see cref="BIGGER"/> или <see cref="EQUALS"/></returns>
+        public int Compare(byte left, byte right)
+        {
+            try
+            {
+                byte val = checked((byte)(left-right));
+            }
+            catch
+            {
+                return SMALLER;
+            }
+            try
+            {
+                byte val = checked((byte)(right-left));
+            }
+            catch
+            {
+                return BIGGER;
+            }
+            return EQUALS;
+        }
+
+        /// <summary>
+        /// Сравнивает два числа
+        /// </summary>
+        /// <param name="left">Левое число</param>
+        /// <param name="right">Правое число</param>
+        /// <returns>Одна из констант <see cref="SMALLER"/>, <see cref="BIGGER"/> или <see cref="EQUALS"/></returns>
+        public int Compare(ushort left, ushort right)
+        {
+            try
+            {
+                ushort val = checked((ushort)(left-right));
+            }
+            catch
+            {
+                return SMALLER;
+            }
+            try
+            {
+                ushort val = checked((ushort)(right-left));
+            }
+            catch
+            {
+                return BIGGER;
+            }
+            return EQUALS;
+        }
+
+        /// <summary>
+        /// Сравнивает два числа
+        /// </summary>
+        /// <param name="left">Левое число</param>
+        /// <param name="right">Правое число</param>
+        /// <returns>Одна из констант <see cref="SMALLER"/>, <see cref="BIGGER"/> или <see cref="EQUALS"/></returns>
+        public int Compare(uint left, uint right)
+        {
+            try
+            {
+                uint val = checked(left-right);
+            }
+            catch
+            {
+                return SMALLER;
+            }
+            try
+            {
+                uint val = checked(right-left);
+            }
+            catch
+            {
+                return BIGGER;
+            }
+            return EQUALS;
+        }
+
+        /// <summary>
+        /// Сравнивает два числа
+        /// </summary>
+        /// <param name="left">Левое число</param>
+        /// <param name="right">Правое число</param>
+        /// <returns>Одна из констант <see cref="SMALLER"/>, <see cref="BIGGER"/> или <see cref="EQUALS"/></returns>
+        public int Compare(ulong left, ulong right)
+        {
+            try
+            {
+                ulong val = checked(left-right);
+            }
+            catch
+            {
+                return SMALLER;
+            }
+            try
+            {
+                ulong val = checked(right-left);
+            }
+            catch
+            {
+                return BIGGER;
+            }
+            return EQUALS;
+        }
+    }
+}
---
+++ src/Libraries/Math/IncrementService.cs
@@ -0,0 +1,82 @@
+﻿using System.Numerics;
+
+namespace Math
+{
+    /// <summary>
+    /// Сервис, реализующий инкремент числа.
+    /// </summary>
+    public class IncrementService
+    {
+        /// <summary>
+        /// Увеличивает значение числа на единицу.
+        /// </summary>
+        /// <param name="val">Значение, которое необходимо увеличить</param>
+        public void Increment(ref byte val) => val++;
+
+        /// <summary>
+        /// Увеличивает значение числа на единицу.
+        /// </summary>
+        /// <param name="val">Значение, которое необходимо увеличить</param>
+        public void Increment(ref ushort val) => val++;
+
+        /// <summary>
+        /// Увеличивает значение числа на единицу.
+        /// </summary>
+        /// <param name="val">Значение, которое необходимо увеличить</param>
+        public void Increment(ref uint val) => val++;
+
+        /// <summary>
+        /// Увеличивает значение числа на единицу.
+        /// </summary>
+        /// <param name="val">Значение, которое необходимо увеличить</param>
+        public void Increment(ref ulong val) => val++;
+
+        /// <summary>
+        /// Увеличивает значение числа на единицу.
+        /// </summary>
+        /// <param name="val">Значение, которое необходимо увеличить</param>
+        public void Increment(ref sbyte val) => val-=-1;
+
+        /// <summary>
+        /// Увеличивает значение числа на единицу.
+        /// </summary>
+        /// <param name="val">Значение, которое необходимо увеличить</param>
+        public void Increment(ref short val) => val-=-1;
+
+        /// <summary>
+        /// Увеличивает значение числа на единицу.
+        /// </summary>
+        /// <param name="val">Значение, которое необходимо увеличить</param>
+        public void Increment(ref int val) => val-=-1;
+
+        /// <summary>
+        /// Увеличивает значение числа на единицу.
+        /// </summary>
+        /// <param name="val">Значение, которое необходимо увеличить</param>
+        public void Increment(ref long val) => val-=-1;
+
+        /// <summary>
+        /// Увеличивает значение числа на единицу.
+        /// </summary>
+        /// <param name="val">Значение, которое необходимо увеличить</param>
+        public void Increment(ref float val) => val-=-1;
+
+        /// <summary>
+        /// Увеличивает значение числа на единицу.
+        /// </summary>
+        /// <param name="val">Значение, которое необходимо увеличить</param>
+        public void Increment(ref double val) => val-=-1;
+
+        /// <summary>
+        /// Увеличивает значение числа на единицу.
+        /// </summary>
+        /// <param name="val">Значение, которое необходимо увеличить</param>
+        public void Increment(ref decimal val) => val-=-1;
+
+        /// <summary>
+        /// Увеличивает значение числа на единицу.
+        /// </summary>
+        /// <param name="val">Значение, которое необходимо увеличить</param>
+        public void Increment(ref BigInteger val) => val-=-1;
+    }
+}
