afc9375835cc556589c8e43cd8efd34592f65581
KirillAldashkin <a89086520092k@yandex.ru>
2021-11-01 00:58:02 +0800

    Decrement service

---
+++ src/Libraries/Math/DecrementService.cs
@@ -0,0 +1,120 @@
+﻿using System.Numerics;
+
+namespace Math
+{
+    /// <summary>
+    /// Сервис, реализующий декремент числа.
+    /// </summary>
+    public class DecrementService
+    {
+        IncrementService increment;
+        NegateService negate;
+
+        /// <summary>
+        /// Создаёт новый экземпляр сервиса декремента числа.
+        /// </summary>
+        /// <param name="increment">Сервис инкремента</param>
+        /// <param name="negate">Сервис инверсии</param>
+        public DecrementService(IncrementService increment, NegateService negate)
+        {
+            this.increment = increment;
+            this.negate = negate;
+        }
+
+        /// <summary>
+        /// Уменьшает значение числа на единицу.
+        /// </summary>
+        /// <param name="val">Значение, которое необходимо уменьшить</param>
+        public void Decrement(ref byte val)
+        {
+            short vl = val;
+            negate.Negate(ref vl);
+            increment.Increment(ref vl);
+            negate.Negate(ref vl);
+            vl = (byte)val;
+        }
+
+        /// <summary>
+        /// Уменьшает значение числа на единицу.
+        /// </summary>
+        /// <param name="val">Значение, которое необходимо уменьшить</param>
+        public void Decrement(ref sbyte val)
+        {
+            negate.Negate(ref val);
+            increment.Increment(ref val);
+            negate.Negate(ref val);
+        }
+
+        /// <summary>
+        /// Уменьшает значение числа на единицу.
+        /// </summary>
+        /// <param name="val">Значение, которое необходимо уменьшить</param>
+        public void Decrement(ref short val)
+        {
+            negate.Negate(ref val);
+            increment.Increment(ref val);
+            negate.Negate(ref val);
+        }
+
+        /// <summary>
+        /// Уменьшает значение числа на единицу.
+        /// </summary>
+        /// <param name="val">Значение, которое необходимо уменьшить</param>
+        public void Decrement(ref ushort val)
+        {
+            int vl = val;
+            negate.Negate(ref vl);
+            increment.Increment(ref vl);
+            negate.Negate(ref vl);
+            val = (ushort)vl;
+        }
+
+        /// <summary>
+        /// Уменьшает значение числа на единицу.
+        /// </summary>
+        /// <param name="val">Значение, которое необходимо уменьшить</param>
+        public void Decrement(ref int val)
+        {
+            negate.Negate(ref val);
+            increment.Increment(ref val);
+            negate.Negate(ref val);
+        }
+
+        /// <summary>
+        /// Уменьшает значение числа на единицу.
+        /// </summary>
+        /// <param name="val">Значение, которое необходимо уменьшить</param>
+        public void Decrement(ref uint val)
+        {
+            long vl = val;
+            negate.Negate(ref vl);
+            increment.Increment(ref vl);
+            negate.Negate(ref vl);
+            val = (uint)vl;
+        }
+
+        /// <summary>
+        /// Уменьшает значение числа на единицу.
+        /// </summary>
+        /// <param name="val">Значение, которое необходимо уменьшить</param>
+        public void Decrement(ref long val)
+        {
+            negate.Negate(ref val);
+            increment.Increment(ref val);
+            negate.Negate(ref val);
+        }
+
+        /// <summary>
+        /// Уменьшает значение числа на единицу.
+        /// </summary>
+        /// <param name="val">Значение, которое необходимо уменьшить</param>
+        public void Decrement(ref ulong val)
+        {
+            BigInteger vl = val;
+            negate.Negate(ref vl);
+            increment.Increment(ref vl);
+            negate.Negate(ref vl);
+            val = (ulong)vl;
+        }
+    }
+}
