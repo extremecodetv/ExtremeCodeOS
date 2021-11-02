645e6269240f0baeb288a68a685bcca59d525249
vulpes-solis <gracedawgzzzzzz@gmail.com>
2021-11-02 22:54:07 +0300

    src/Biboran: добавлена читалка.

--- src/Biboran/biboran.c
+++ src/Biboran/biboran.c
@@ -1,6 +1,7 @@
 
 #include<gtk/gtk.h>
 #include<stdio.h>
+#include<stdlib.h>
 
 enum __MAIN_SIZE{ _W=500,
                   _H=500 }; // Main window size.
@@ -8,43 +9,68 @@ enum __MAIN_SIZE{ _W=500,
 enum __SUB_SIZE{ _SUB_W=1000,
                  _SUB_H=700 }; // Sub window size.
 
+#define _ICON    "res/icon.png"
+#define _IMAGE   "res/abdul.png"
+#define _BIBORAN "res/biboran.txt"
+
 #ifndef _string
 typedef char* _string;
 #endif
 
-#define DEBUG
+#define DEBUG // Debug mode with some extra logs.
+
+#define __BUFFER_FAILED(buffer)\
+    do {\
+        buffer = (_string)malloc(1);\
+        memset(buffer, 0, sizeof(buffer));\
+        return buffer;\
+    } while(0)
+
+#define __FREE_CLOSE(file, buffer)\
+    do {\
+        free(buffer);\
+        fclose(file);\
+    } while(0)
 
 
-// General function for window initialisation.
-GtkWidget* window_init(int _w, int _h, _string title) {
-    static GtkWidget* win;
-    win = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-    gtk_window_set_title(GTK_WINDOW (win), title);
-    gtk_window_set_default_size(GTK_WINDOW (win), _w, _h);
-    gtk_window_set_position(GTK_WINDOW (win), GTK_WIN_POS_CENTER);
-#ifdef DEBUG
-    fprintf(stdout, "[window_init] function call, window %dx%d, title %s\n",
-            _w, _h, title);
-#endif
-    return win;
+// Get content from some file and write it to buffer.
+_string get_buffer_from_file(_string filename) {
+    static _string buffer;
+    FILE* file = fopen(filename, "r");
+    
+    if (file == NULL) {
+        fprintf(stderr, "Failed to read \"%s\"\n.", filename);
+        __BUFFER_FAILED(buffer);    
+    }
+
+    if (fseek(file, 0L, SEEK_END) == 0) {
+        long b_size = ftell(file);
+        if (b_size == -1) {
+            fprintf(stderr, "Failed to get file length.\n");
+            fclose(file);
+            __BUFFER_FAILED(buffer);
+        }
+        buffer = (_string)malloc(sizeof(char)*b_size+1);
+        if (fseek(file, 0L, SEEK_SET) != 0) {
+            fprintf(stderr, "Failed to get back to the begining of the file.\n");
+            __FREE_CLOSE(file, buffer);
+            __BUFFER_FAILED(buffer);
+        }
+
+        size_t n_l = fread(buffer, sizeof(char), b_size, file);
+        if (ferror(file) != 0) {
+            fprintf(stderr, "Error occured while reading from file to buffer!\n");
+            __FREE_CLOSE(file, buffer);
+            __BUFFER_FAILED(buffer);
+        }
+        else
+            buffer[n_l+1] = 0;
+    }
+
+    fclose(file);
+    return buffer;
 }
 
-// Destroy callback function for read window.
-void read_window_destroy(GtkWidget* p_wid, gpointer _) {
-    gtk_widget_destroy(p_wid);
-#ifdef DEBUG
-    puts("[read_window_destroy] function call.");
-#endif
-}
-
-// Create read window widget.
-void read_window_create(GtkWidget* p_wid, gpointer data) {
-    GtkWidget* read_win = window_init(_SUB_W, _SUB_H, "Священный Биборан.");
-    // When window is closed destroy it's widget.
-    g_signal_connect(read_win, "destroy",
-            G_CALLBACK(read_window_destroy), NULL);
-    gtk_widget_show_all(read_win);
-}
 
 // Default function to create GDK pixbuffers from files.
 GdkPixbuf* create_pixbuf_from_file(const gchar* filename) {
@@ -60,11 +86,88 @@ GdkPixbuf* create_pixbuf_from_file(const gchar* filename) {
     return pixbuf;
 }
 
+// General function for window initialisation.
+GtkWidget* window_init(int _w, int _h, _string title) {
+    static GtkWidget* win;
+    win = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_title(GTK_WINDOW (win), title);
+    gtk_window_set_default_size(GTK_WINDOW (win), _w, _h);
+    gtk_window_set_position(GTK_WINDOW (win), GTK_WIN_POS_CENTER);
+#ifdef DEBUG
+    fprintf(stdout, "[window_init] function call, window %dx%d, title %s\n",
+            _w, _h, title);
+#endif
+    return win;
+}
+
+void window_set_icon(GtkWidget* window, const gchar* filename) {
+    GdkPixbuf* icon;
+    icon = create_pixbuf_from_file(filename);
+    gtk_window_set_icon(GTK_WINDOW (window), icon);
+    g_object_unref(icon);
+}
+
+// Destroy callback function for read window.
+void read_window_destroy(GtkWidget* p_wid, gpointer _) {
+    gtk_widget_destroy(p_wid);
+#ifdef DEBUG
+    fprintf(stdout, "[read_window_destroy] function call.\n");
+#endif
+}
+
+// Create read window widget.
+void read_window_create(GtkWidget* p_wid, gpointer data) {
+    GtkWidget* read_win = window_init(_SUB_W,
+                                      _SUB_H, 
+                                      "Священный Биборан.");
+    GtkWidget* scrolled_read = gtk_scrolled_window_new(NULL, NULL);
+    gtk_widget_show(scrolled_read);
+
+    // When window is closed destroy it's widget.
+    g_signal_connect(read_win,
+                     "destroy",
+                     G_CALLBACK(read_window_destroy),
+                     NULL);
+    
+    window_set_icon(read_win, _ICON);
+
+    static _string buffer;
+    buffer = get_buffer_from_file(_BIBORAN);
+#ifdef DEBUG
+    fprintf(stdout, "%s\n", buffer);
+#endif
+
+    GtkTextBuffer* content;
+    content = gtk_text_buffer_new(NULL);
+    gtk_text_buffer_set_text(content, buffer, -1);
+
+    GtkWidget* text_area;
+    text_area = gtk_text_view_new_with_buffer(content);
+    gtk_widget_show_all(text_area);
+
+    GtkWidget* read_frame;
+    read_frame = gtk_frame_new("Анша Абдуль!");
+    gtk_widget_show(read_frame);
+
+    gtk_container_add(GTK_CONTAINER (read_win),
+                     scrolled_read);
+    gtk_container_add(GTK_CONTAINER (scrolled_read),
+                       read_frame);
+    gtk_container_add(GTK_CONTAINER (read_frame),
+                     text_area);
+
+    gtk_widget_show_all(read_win);
+}
+
 // Initialise button with label and pack it to the general box.
 GtkWidget* init_and_pack_button(GtkWidget* box, _string label) {
     static GtkWidget* button;
     button = gtk_button_new_with_label(label);
-    gtk_box_pack_start(GTK_BOX (box), button, TRUE, TRUE, 0);
+    gtk_box_pack_start(GTK_BOX (box),
+                       button,
+                       TRUE,
+                       TRUE,
+                       0);
     return button;
 }
 
@@ -74,9 +177,12 @@ GtkWidget* init_frame_with_shadow(_string label, GtkWidget* container) {
     frame = gtk_frame_new(label);
     gtk_widget_show(frame);
 
-    gtk_frame_set_shadow_type(GTK_FRAME (frame), GTK_SHADOW_IN);
-    gtk_frame_set_label_align(GTK_FRAME (frame), .5, 0);
-    gtk_container_add(GTK_CONTAINER (container), frame);
+    gtk_frame_set_shadow_type(GTK_FRAME (frame),
+                              GTK_SHADOW_IN);
+    gtk_frame_set_label_align(GTK_FRAME (frame),
+                             .5, 0);
+    gtk_container_add(GTK_CONTAINER (container),
+                      frame);
 
     return frame;
 }
@@ -87,16 +193,15 @@ int main(int argc, char** argv) {
     gtk_init(&argc, &argv);
 
     GtkWidget* win;
-    win = window_init(_W, _H, "Biboran");
+    win = window_init(_W,
+                      _H,
+                      "Biboran");
+    window_set_icon(win, _ICON);
 
-    g_signal_connect(win, "destroy",
-            G_CALLBACK (gtk_main_quit), NULL);
-
-    // Application icon.
-    GdkPixbuf* icon;
-    icon = create_pixbuf_from_file("res/icon.png");
-    gtk_window_set_icon(GTK_WINDOW (win), icon);
-    g_object_unref(icon);
+    g_signal_connect(win,
+                    "destroy",
+                    G_CALLBACK (gtk_main_quit),
+                    NULL);
 
     // Main Vbox init.
     GtkWidget* main_vbox;
@@ -109,7 +214,7 @@ int main(int argc, char** argv) {
 
     // Abdul'
     GtkWidget* abdul;
-    abdul = gtk_image_new_from_file("res/abdul.png");
+    abdul = gtk_image_new_from_file(_IMAGE);
     gtk_widget_show(abdul);
     gtk_container_add(GTK_CONTAINER (top_frame), abdul);
 
@@ -126,14 +231,18 @@ int main(int argc, char** argv) {
     // Read button.
     GtkWidget* read_but;
     read_but = init_and_pack_button(sub_hbox, "ЧИТАТЬ");
-    g_signal_connect(read_but, "clicked",
-            G_CALLBACK (read_window_create), NULL);
+    g_signal_connect(read_but,
+                    "clicked",
+                    G_CALLBACK (read_window_create),
+                    NULL);
 
     // Quit button.
     GtkWidget* quit_but;
     quit_but = init_and_pack_button(sub_hbox, "ВЫЙТИ");
-    g_signal_connect(quit_but, "clicked",
-            G_CALLBACK (gtk_main_quit), NULL);
+    g_signal_connect(quit_but,
+                    "clicked",
+                     G_CALLBACK (gtk_main_quit),
+                     NULL);
 
     // Show main window.
     gtk_widget_show_all(win);
