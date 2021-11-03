107010dc71c23c789ca2e1409bc4a6163314a566
maxsssssssss <51308244+maxsssssssss@users.noreply.github.com>
2021-11-03 20:25:53 +0300

    Компилятор (#110)
    * Компилятор
    
    Компилятор
    
    * Доп. программы.

---
+++ Compiler/src/CompilerPoint.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExtremeCodeOSCompiler
+{
+    struct CompilerPoint
+    {
+        public string CompilType;
+        public Dictionary<string, string> CompilData;
+    }
+}
