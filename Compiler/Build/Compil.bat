2f3db09311aa06af570406d5ea73069700efde6d
Nikita <nikita.d161@gmail.com>
2021-11-04 15:54:32 +0300

    merging

---
+++ Compiler/Build/Compil.bat
@@ -0,0 +1,5 @@
+cd ..
+cd ..
+mkdir "OS/Games/ExtremeCodeOS runner" & gcc "src/Games/ExtremeCodeOS runner/runner.c" -O1 -o "OS/Games/ExtremeCodeOS runner/Runner"
+mkdir "OS/Kernel" & gcc "src/Kernel/kernel.c" -O1 -o "OS/Kernel/kernel"
+mkdir "OS/brainfuck" & gcc "src/brainfuck/inter.c" -O1 -o "OS/brainfuck/inter"
